f027c007b040890c756c7c3078ae1a78887216c9
Meija Lohiniva <meijastiina@gmail.com>
2026-02-12 13:22:18 +0200

    20260212

--- first.js
+++ first.js
@@ -1,1 +1,9 @@
-alert("Hello from first!");
+// alert("Hello from first!");
+
+let pElements = document.getElementsByTagName("p");
+// for( let i = 0; i < pElements.length; i++) {
+//     pElements[i].innerHTML = "This has been updated";
+// }
+// [...pElements].forEach(element => {
+//     element.innerHTML = "This has been updated";
+// });
