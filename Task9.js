b3bf3582eaa23a24449a38256c61a696849df40f
Meija Lohiniva <meijastiina@gmail.com>
2026-02-18 12:47:55 +0200

    Task9 (not completed)

---
+++ Task9.js
@@ -0,0 +1,54 @@
+const cameraData = [
+        {
+          model: 'EOS 850D',
+          image: 'images/eos-850d.jpg',
+          highlights: [
+            '24.1 megapixels',
+            '10 frames per second',
+            '4K video',
+            '45 point AF',
+            'Digic 8'
+          ]
+        },
+        {
+          model: 'EOS 90D',
+          image: 'images/eos-90d.jpg',
+          highlights: [
+            '32.5 megapixels',
+            '10 frames per second',
+            '4K video',
+            '1300 frames per battery',
+            'ISO 25600'
+          ]
+        },
+        {
+          model: 'EOS M6 Mark II',
+          image: 'images/eos-m6-mark-ii.jpg',
+          highlights: [
+            '32.5 megapixels',
+            '14 frames per second',
+            '408 grams',
+            '1300 frames per battery',
+            'ISO 25600'
+          ]
+        },
+        {
+          model: 'Powershot G7 X Mark III',
+          image: 'images/powershot-g7-x-mark-iii.jpg',
+          highlights: [
+            '3.5mm microphone input',
+            '120fps FULL HD video',
+            '20.1 megapixels',
+            'Tilt LCD',
+            '24mm - 4.2x Zoom'
+          ]
+        },
+      ];
+
+      /* Your task is to write a solution which takes the above data and
+         creates DOM elements based on the data in the same manner as in previous exercise
+         with single product. All four products should be created when the button with text
+         "Show all products with Create Element" or "... with Inner HTML" is clicked.
+
+         Do two versions of the solution. One with createElement and one with innerHTML.
+        */
